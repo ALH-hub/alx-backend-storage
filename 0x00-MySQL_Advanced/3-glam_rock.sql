a927ee1de78a31d5c31e641d5d8a2e427aa53bd4
Alhadji Oumate <alhadjioum0@gmail.com>
2024-05-10 00:26:10 +0100

    query

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- lists all bands with Glam rock as their main style, ranked by their longevity
 
-SELECT band_name, (split - formed) AS longevity
+SELECT band_name, ifnull(split, 2020)-ifnull(formed, 0) AS lifespan
 FROM metal_bands
-WHERE style = 'Glam rock'
-ORDER BY longevity DESC;
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
