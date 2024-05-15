2ff84707ea256a5df1c8b8402d04698df36239d3
Alhadji Oumate <alhadjioum0@gmail.com>
2024-05-15 23:17:57 +0100

    Reddis instance

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,1 @@
+ALX course: learning reddis basics and use it as a simple cache
--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,10 +8,12 @@ from typing import Union
 
 class Cache:
     def __init__(self):
+        """initialize redis connection and flush the db"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """generate a random key and store the data in redis"""
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
