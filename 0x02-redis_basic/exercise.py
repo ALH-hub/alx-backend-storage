06aa211975a2b23a129e72c6dd02f18e85373199
Alhadji Oumate <alhadjioum0@gmail.com>
2024-05-15 23:33:32 +0100

    Reddis instance

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,19 +17,19 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
-    
-    def get(self, key: str, fn: Optional[Callable] = None) \
-        -> Union[str, bytes, int, float]:
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[
+            str, bytes, int, float]:
         """get the data from redis and apply the function if it exists"""
         data = self._redis.get(key)
         if fn:
             return fn(data)
         return data
-    
+
     def get_str(self, val: bytes) -> str:
         """return string representation of val from get method """
         return val.decode('utf-8')
-    
+
     def get_int(self, val: bytes) -> int:
         """return integer representation of val from get method """
         return int.from_bytes(val, byteorder='big')
